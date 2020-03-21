73aae4fa2f836501e1a7499a95ef8c83b1153110
Fırat Özden <36071011+fir4tozden@users.noreply.github.com>
2020-03-21 21:14:16 +0300

    Update js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3.py

--- js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3.py
+++ js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3.py
@@ -28,9 +28,9 @@ while True:
             break;
         else:
             pass;
-     sock.sendto(bytes, (ip, port));
      sent = sent + 1;
      port = port + 1;
+     sock.sendto(bytes, (ip, port));
      print "\033[1;33;40m<--", time.ctime(time.time()), "-->  \033[1;36;40m<-- %s:%s -->\033[0;37;40m" %(ip, port);
      if port == 65534:
        port = 1;
