bdb5eea05ad2764ad358663bb663b04a95d0a565
Fırat Özden <36071011+fir4tozden@users.noreply.github.com>
2020-03-21 20:29:40 +0300

    Update js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3.py

--- js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3.py
+++ js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3.py
@@ -21,7 +21,6 @@ print ("""
 \033[0;37;40m""");
 ip = raw_input("\033[1;36;40mIP: \033[1;33;40m");
 port = input("\033[1;36;40mPort: \033[1;33;40m");
-print("\033[0;37;40m");
 os.system("clear");
 sent = 0;
 while True:
@@ -33,6 +32,6 @@ while True:
      sock.sendto(bytes, (ip, port));
      sent = sent + 1;
      port = port + 1;
-     print "\033[1;33;40m<--!%s-->     \033[1;36;40m<--%s:%s-->\033[0;37;40m" %(sent, ip, port);
+     print "\033[1;33;40m<--%s-->   \033[1;36;40m<--%s:%s-->\033[0;37;40m" %(sent, ip, port);
      if port == 65534:
        port = 1;
